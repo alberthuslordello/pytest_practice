f09f2952a86077afe7aacd6cbfc17d74835d8b3b
Alberthus Lordello <athuskoops@gmail.com>
2022-09-06 16:53:24 -0300

    re-made convert_to_int.py and test_convert_to_int.py through TDD methodology.

--- pytest_practice/convert_to_int.py
+++ pytest_practice/convert_to_int.py
@@ -1,7 +1,15 @@
-def convert_to_int (value):
-    if not isinstance(value, str):
-        if isinstance(value, int):
-            return value
-        else:
-            raise TypeError("is not an string or integer")
-    return int(value.replace(',', ''))
+def convert_to_int(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+    integer_string_without_commas = "".join(comma_separated_parts)
+    try:
+        return int(integer_string_without_commas)
+    # Fill in with a ValueError
+    except ValueError:
+        return None
--- tests/test_convert_to_int.py
+++ tests/test_convert_to_int.py
@@ -1,22 +1,52 @@
 import pytest
 from pytest_practice.convert_to_int import convert_to_int
 
-def test_on_string_with_one_comma():
-    return_value = convert_to_int("2,081")
-    assert isinstance(return_value, int)
-    assert return_value == 2081
 
+################### Normal arguments ############################
+
+def test_with_no_comma():
+    actual = convert_to_int("756")
+    # Complete the assert statement
+    assert actual==756, "Expected: 756, Actual: {0}".format(actual)
+
+def test_with_one_comma():
+    actual = convert_to_int("2,081")
+    # Complete the assert statement
+    assert actual==2081, "Expected: 2081, Actual: {0}".format(actual)
+
+def test_with_two_commas():
+    actual = convert_to_int("1,034,891")
+    # Complete the assert statement
+    assert actual==1034891, "Expected: 1034891, Actual: {0}".format(actual)
+
+
+################ Special arguments #############################
+
+def test_on_string_with_missing_comma():
+    actual = convert_to_int("178100,301")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_string_with_incorrectly_placed_comma():
+    # Assign to the actual return value for the argument "12,72,891"
+    actual = convert_to_int("12,72,891")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_float_valued_string():
+    actual = convert_to_int("23,816.92")
+    # Complete the assert statement
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+"""
 def test_with_int():
     return_value = convert_to_int(2081)
     assert isinstance(return_value, int)
     assert return_value == 2081
-
-
-def test_wit_invalid():
+"""
+#####################  Bad arguments #########################
+""" no bad arguments
+def test_wit_invalid_type():
     with pytest.raises(TypeError):
         return_value = convert_to_int(2081.1)
     with pytest.raises(TypeError):
         return_value = convert_to_int([1234,1234])
-
-# will make an example with TDD latter
+"""
 
