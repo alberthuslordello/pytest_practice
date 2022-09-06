b57d6c8510f7d20b159217628ef0df4e2e6f0a9f
Alberthus Lordello <athuskoops@gmail.com>
2022-09-05 23:03:06 -0300

    Changes to be committed: 	modified:   test_row_to_list.py: improved test coverage for bad, special and normal arguments 	modified:   test_convert_to_int.py: test for exception 	new file:   README.md: file explaining what each file is for 	new file:   row_to_list_cases.pdf: image showing all the test cases to be tested on test_row_to_list.py

---
+++ tests/README.md
@@ -0,0 +1,7 @@
+## row_to_list_cases.pdf
+on any function call of row_to_list all possible use cases are measured by the number of tabs n, and if there are missing values (1 - True, 0 False) forming a tuple (n, m) which then analyzed in each case belonging to either special case, boundary case, and normal case.
+
+
+## test_row_to_list.py
+the code in which the abovementioned tests are written.
+
--- tests/test_convert_to_int.py
+++ tests/test_convert_to_int.py
@@ -13,6 +13,7 @@ def test_with_int():
 
 
 def test_wit_invalid():
-    return_value = convert_to_int(2081.1)
-    assert isinstance(return_value, TypeError) # use pytest.raises(Exception)
-    # link https://stackoverflow.com/questions/23337471/how-to-properly-assert-that-an-exception-gets-raised-in-pytest
+    with pytest.raises(TypeError):
+        return_value = convert_to_int(2081.1)
+    with pytest.raises(TypeError):
+        return_value = convert_to_int([1234,1234])
